673b403fafdb5827d892d2736da9e92e9e932618
DuAn1 <thanhboi312@gmail.com>
2021-11-15 15:05:32 +0700

    a

--- MVC/views/master.php
+++ MVC/views/master.php
@@ -2,20 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./mvc/public/css/style.css">
-    <link rel="stylesheet" href="./mvc/public/css/reset.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
-    <script src=" https://unpkg.com/sweetalert/dist/sweetalert.min.js "> </script>
-    <title>Nguyễn Đăng Thành</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dự án 1 - Nhóm 2</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/MVC/public/css/grid.css" />
+    <link rel="stylesheet" href="<?= BASE_URL ?>/MVC/public/css/base.css" />
+    <link rel="stylesheet" href="<?= BASE_URL ?>/MVC/public/css/style.css" />
+    <link rel="stylesheet" href="<?= BASE_URL ?>/MVC/public/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/MVC/public/themify-icons/themify-icons.css" />
 </head>
 
 <body>
-    <?php require_once "./mvc/views/blocks/header.php"; ?>
-    <?php require_once "./mvc/views/Page/Site/" . $data["Page"] . ".php" ?>
-    <?php require_once "./mvc/views/blocks/footer.php"; ?>
+    <div class="app">
+        <?php require_once "./mvc/views/blocks/header.php"; ?>
+        <?php require_once "./mvc/views/Page/Site/" . $data["Page"] . ".php" ?>
+        <?php require_once "./mvc/views/blocks/footer.php"; ?>
+    </div>
+    <!-- ============================= Javascript ===================================== -->
+    <script src="<?= BASE_URL ?>/MVC/public/js/sroll.js"></script>
+    <!-- <script src="./assets/js/slider.js"></script> -->
 </body>
+
 </html>
