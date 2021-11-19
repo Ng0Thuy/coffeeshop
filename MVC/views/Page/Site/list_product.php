6bee0d9b2136742cabc00f6beb80e1565181a555
quangdzu <nvu012030@gmail.com>
2021-11-19 23:47:50 +0700

    Add list product

--- MVC/views/Page/Site/list_product.php
+++ MVC/views/Page/Site/list_product.php
@@ -6,48 +6,70 @@
         </ul>
     </nav>
     <div class="row sm-gutter app__content">
-        <div class="col l-2">
+        <div class="col l-2-4">
             <div class="slidebar">
                 <div class="slidebar-menu">
                     <h3 class="slidebar-menu__heading">Thực đơn</h3>
                     <div class="slidebar-menu__category">
                         <div class="category-item">
-                            <input type="checkbox" name="" id="" checked style="color: var(--primary-color);">
-                            <span class="category-name">Trà sữa</span>
+                            <label class="checkbox">
+                                <input type="checkbox" name="">
+                                <i class="icon-checkbox"></i>
+                                <span class="category-name">Trà sữa</span>
+                            </label>
                         </div>
 
                         <div class="category-item">
-                            <input type="checkbox" name="" id="">
-                            <span class="category-name">Trà nguyên chất</span>
+                            <label class="checkbox">
+                                <input type="checkbox" name="">
+                                <i class="icon-checkbox"></i>
+                                <span class="category-name">Trà nguyên chất</span>
+                            </label>
                         </div>
 
                         <div class="category-item">
-                            <input type="checkbox" name="" id="">
-                            <span class="category-name">Cà phê</span>
+                            <label class="checkbox">
+                                <input type="checkbox" name="">
+                                <i class="icon-checkbox"></i>
+                                <span class="category-name">Cà phê</span>
+                            </label>
                         </div>
+                    
 
-                        <div class="category-item">
-                            <input type="checkbox" name="" id="">
+                    <div class="category-item">
+                        <label class="checkbox">
+                            <input type="checkbox" name="">
+                            <i class="icon-checkbox"></i>
                             <span class="category-name">Nước trái cây</span>
-                        </div>
+                        </label>
+                    </div>
 
-                        <div class="category-item">
-                            <input type="checkbox" name="" id="">
+                    <div class="category-item">
+                        <label class="checkbox">
+                            <input type="checkbox" name="">
+                            <i class="icon-checkbox"></i>
                             <span class="category-name">Thức uống đá xay</span>
-                        </div>
+                        </label>
+                    </div>
 
-                        <div class="category-item">
-                            <input type="checkbox" name="" id="">
+                    <div class="category-item">
+                        <label class="checkbox">
+                            <input type="checkbox" name="">
+                            <i class="icon-checkbox"></i>
                             <span class="category-name">Topping</span>
-                        </div>
+                        </label>
+                    </div>
                     </div>
                 </div>
-                <div class="slidebar-filter__price"></div>
-                <div class="slidebar-favorite__product"></div>
             </div>
+            <div class="slidebar-filter__price">
+                
+            </div>
+            <div class="slidebar-favorite__product"></div>
         </div>
-        <div class="col l-10">
+    </div>
+    <div class="col l-10">
 
-        </div>
+    </div>
     </div>
 </main>
