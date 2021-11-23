aeda52056c086bedfafa75972a4c03922745a207
quangdzu <nvu012030@gmail.com>
2021-11-23 23:00:49 +0700

    Done User

--- MVC/views/Page/Site/user.php
+++ MVC/views/Page/Site/user.php
@@ -1,26 +1,47 @@
-<main class="grid wide container user">
+<main class="grid wide user" style="margin: 100px auto 20px;">
     <div class="info-user">
         <h2 class="info-user__heading">Thông tin khách hàng</h2>
-        <div class="form-info-user">
-            <form action="" method="" class="change-password">
+        <div class="form-info-user row">
+            <form action="" method="" class="col l-3 change-password">
                 <h2 class="change-password__heading">Đổi mật khẩu</h2>
                 <div class="change-password__main">
                     <div class="change-password__item">
                         <p class="change-password__text">Mật khẩu cũ <sup style="color:red;">*</sup></p>
-                        <input type="password" name="" id="">
+                        <input type="password" name="" id="" class="change-password__input" placeholder="Nhập mật khẩu cũ">
                         <!-- <p class="error">Mật không đúng</p> -->
                     </div>
                     <div class="clear"></div>
-
+                    
                     <div class="change-password__item">
                         <p class="change-password__text">Mật khẩu mới <sup style="color:red;">*</sup></p>
-                        <input type="password" name="" id="">
+                        <input type="password" name="" id="" class="change-password__input" placeholder="Nhập mật khẩu mới">
                         <span class="error"></span>
                     </div>
                 </div>
+                <button class="btn btn--success change-password__btn">Thay đổi</button>
             </form>
-            <form action="" method="" class="info-user__main">
+            <form action="" method="" class="col l-9 info-user__main">
+                <div class="info-user__main-item">
+                    <p class="info-user__text">Tên <sup style="color:red;">*</sup></p>
+                    <input type="text" name="" id="" class="info-user__input" value="Nguyễn Đăng Thành" placeholder="Nhập tên của bạn">
+                </div>
 
+                <div class="info-user__main-item">
+                    <p class="info-user__text">Địa chỉ <sup style="color:red;">*</sup></p>
+                    <input type="text" name="" id="" class="info-user__input" value="Đội 3, Làng Cổ Lũy, Xã Hải Ba, Huyện Hải Lăng, Tỉnh Quảng Trị" placeholder="Nhập địa chỉ của bạn">
+                </div>
+                
+                <div class="info-user__main-item contact-info__list">
+                    <div class="contact-info__item">
+                        <p class="info-user__text">Số điện thoại <sup style="color:red;">*</sup></p>
+                        <input type="text" name="" id="" class="info-user__input" value="0384387578" placeholder="Nhập số điện thoại của bạn">
+                    </div>
+                    <div class="contact-info__item">
+                        <p class="info-user__text">Email <small style="color:red;">(Không thay đổi)</small></p>
+                        <input type="text" name="" id="" class="info-user__input" value="thanhboi312@gmail.com" disabled>
+                    </div>
+                </div>
+                <button class="btn btn--success info-user__main--btn">Cập nhật</button>
             </form>
         </div>
     </div>
